76b00b470a752873ad3e12bb587b9ffbbe94dea5
xfinger <xfinger@gmail.com>
2018-09-29 17:12:43 -0400

    chads edits

--- src/app/listitem/listitem.component.ts
+++ src/app/listitem/listitem.component.ts
@@ -1,15 +1,19 @@
-import { Component, 
-         Input, 
-         OnInit, 
-         OnChanges, 
-         SimpleChanges, 
-         SimpleChange,
-         ChangeDetectionStrategy,
-         OnDestroy  } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnInit,
+  OnChanges,
+  SimpleChanges,
+  SimpleChange,
+  ChangeDetectionStrategy,
+  OnDestroy
+} from '@angular/core';
 import { Listitem } from '../model/listitem.model';
-import { Router, 
-         ActivatedRoute, 
-         ParamMap } from '@angular/router'; 
+import {
+  Router,
+  ActivatedRoute,
+  ParamMap
+} from '@angular/router';
 import { ListitemService } from './listitem.service';
 import { Location } from '@angular/common';
 
@@ -18,49 +22,57 @@ import { Location } from '@angular/common';
   templateUrl: './listitem.component.html',
   styleUrls: ['./listitem.component.scss']
 })
-export class ListitemComponent implements OnChanges, OnInit {
-   constructor(
+export class ListitemComponent implements OnInit {
+  constructor(
     private route: ActivatedRoute,
-    private router: Router, 
+    private router: Router,
     private listitemService: ListitemService,
     private location: Location,
-    
-    ){} 
-     
+
+  ) { }
+
   listitems: Listitem;
-  @Input() listId ;
-  
-    ngOnInit(): void  {
+  chadListItems: Listitem[];
+  @Input() listId;
+
+  ngOnInit(): void {
     this.getListitems();
     console.log("-----------------" + this.listId)
   }
 
-  ngOnChanges(changes: SimpleChanges): void{
-    console.log('OnChanges');
-    console.log(JSON.stringify(changes));
-    //@input changes call getListitems to update listitems
-     this.getListitems();
+  // ngOnChanges(changes: SimpleChanges): void {
+  //   console.log('OnChanges');
+  //   console.log(JSON.stringify(changes));
+  //   //@input changes call getListitems to update listitems
+  //   this.getListitems();
 
-  } 
+  // }
 
 
-getListitems(): void {
-     
+  getListitems() {
     //const id = + this.route.snapshot.paramMap.get('id');
     this.listitemService.getListitems(this.listId)
-      .subscribe(listitems => this.listitems = listitems);
+      .subscribe((listitems) => {
+        this.listitems = listitems;
+        console.log('this.listitems object is ', this.listitems);
+      });
+  }
+  // getListitem(): void {
+  //   const id = + this.route.snapshot.paramMap.get('id');
+  //   this.listitemService.getListitem(id)
+  //     .subscribe(listitems => this.listitems = listitems);
+  //     // .subscribe(listitems => this.listitems.push(listitems));
+  //     this.chadListItems.push(this.listitems);
+  //     console.log('chads list = ', this.chadListItems);
+  // }
+
+  //button to delete listitem
+  deleteListitem(id) {
+    this.listitemService.deleteListitem(id).subscribe();
+    // this.listitems.splice(0);
+    this.listitems
+
   }
 
-  getListitem(): void {
-    const id = + this.route.snapshot.paramMap.get('id');
-    this.listitemService.getListitem(id)
-      .subscribe(listitems => this.listitems = listitems);
-  }
 
-   //button to delete listitem
-    deleteListitem(id){
-        this.listitemService.deleteListitem(id).subscribe();
-  }
-
-  
 }
